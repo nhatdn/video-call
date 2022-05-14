c57ef54ce155b72ee7d39c91969fe75032021e87
nhatdn <nhatdn.it@gmail.com>
2022-05-14 12:15:29 +0700

    update css

--- src/share.js
+++ src/share.js
@@ -3,8 +3,11 @@ import Peer from "peerjs";
 const another = document.getElementById("another-peers-id");
 const shareBtn = document.getElementById("share");
 const callBtn = document.getElementById("call");
+const myID = document.querySelector(".my-id");
 
-function openStreamDisplayMedia(config = { audio: true, video: true }) {
+function openStreamDisplayMedia(
+  config = { video: { mediaSource: "screen" }, audio: true }
+) {
   return navigator.mediaDevices.getDisplayMedia(config);
 }
 
@@ -13,15 +16,22 @@ function openStream(config = { audio: true, video: true }) {
   return navigator.mediaDevices.getUserMedia(config);
 }
 
-function playStream(id, stream) {
+function playStream(id, stream, calling = true) {
   const video = document.getElementById(id);
+  if (calling) {
+    video.classList.add("call");
+  }
+  console.log(stream);
   video.srcObject = stream;
   video.play();
 }
 
 export default function bar() {
   var peer = new Peer();
-  peer.on("open", (id) => console.log(id));
+  peer.on("open", (id) => {
+    myID.innerText = `Your ID: ${id}`;
+    console.log(id);
+  });
 
   callBtn.addEventListener("click", () => {
     const id = another.value;
@@ -35,7 +45,7 @@ export default function bar() {
   shareBtn.addEventListener("click", () => {
     const id = another.value;
     openStreamDisplayMedia().then((stream) => {
-      playStream("local", stream);
+      playStream("local", stream, false);
       const call = peer.call(id, stream);
       call.on("stream", (remoteStream) => playStream("remote", remoteStream));
     });
@@ -47,14 +57,14 @@ export default function bar() {
     );
     if (result) {
       openStreamDisplayMedia().then((stream) => {
-        playStream("local", stream);
+        playStream("local", stream, false);
         call.answer(stream);
         call.on("stream", (remoteStream) => playStream("remote", remoteStream));
       });
     } else {
       openStream().then((stream) => {
         call.answer(stream);
-        playStream("local", stream);
+        playStream("local", stream, false);
         call.on("stream", (remoteStream) => playStream("remote", remoteStream));
       });
     }
